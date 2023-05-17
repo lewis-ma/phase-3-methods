69e247dfcc643e0069e24ca4cdc06eed2305dcc9
lewisky <lewisky28mawira@gmail.com>
2023-05-17 21:20:11 +0300

    complete assignmemt

--- methods.rb
+++ methods.rb
@@ -1,1 +1,39 @@
 # Your code here!
+
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+greet_with_default
+greet_with_default("Sunny")
+
+def add(num1, num2)
+  return num1 + num2
+end
+
+sum = add(1, 2)
+puts sum
+
+def halve(number)
+  return nil unless number.is_a?(Numeric)
+  
+  return number / 2
+end
+
+result = halve(4)
+puts result
+
+result = halve("two")
+puts result
